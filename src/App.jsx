7e90ea5037166d59f95337e57da6a91239c40dd9
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-06-05 20:15:10 -0500

    Set loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
+import { useState } from 'react'
+
 const App = () => {
+
+	const [user, setUser] = useState({})
+	const [loader, setLoader] = useState(false)
+
+	const fetchUser = async () => {
+		try {
+			setLoader(!loader)
+			const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+			const data = await response.json()
+			setUser(data)
+		} catch (error) {
+			console.error(error)
+		} finally {
+			setTimeout(() => {
+				setLoader(false)
+			}, 2000)
+		}
+	}
+
+	console.log({ user })
 	return (
-		<h1>React Contex API</h1>
+		<main>
+			<div className="container mx-auto py-4">
+				<h1 className='text-4xl text-center font-semibold text-zinc-500 mb-4'>React Contex API</h1>
+				<section className='py-4 flex gap-4 justify-evenly'>
+					<article>
+						{
+							loader
+								? <pre className='font-semibold'>Loading...</pre>
+								: <pre>{JSON.stringify(user, null, 2)}</pre>
+						}
+					</article>
+					<article>
+						<button
+							className='py-2 px-4 shadow bg-orange-500 border border-orange-500 rounded text-white active:scale-95 focus:outline-none'
+							onClick={fetchUser}
+						>
+							Update
+						</button>
+					</article>
+				</section>
+			</div>
+		</main>
 	)
 }
 
